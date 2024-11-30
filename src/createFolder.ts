e9406628b4a3e8ff9c516a5132e006b91cd2ecc2
Prince Raj <princerajchy.1733@gmail.com>
2024-11-30 14:40:12 +0545

    filefolderreplace

---
+++ src/createFolder.ts
@@ -0,0 +1,22 @@
+// createFolder.ts
+import * as vscode from 'vscode';
+import * as fs from 'fs';
+import * as path from 'path';
+
+export function createFolder() {
+    vscode.window.showInputBox({ prompt: 'Enter folder name' }).then(folderName => {
+        if (folderName) {
+            const workspaceFolders = vscode.workspace.workspaceFolders;
+            if (workspaceFolders) {
+                const folderPath = path.join(workspaceFolders[0].uri.fsPath, folderName);
+                fs.mkdir(folderPath, (err) => {
+                    if (err) {
+                        vscode.window.showErrorMessage('Failed to create folder: ' + err.message);
+                    } else {
+                        vscode.window.showInformationMessage('Folder created: ' + folderPath);
+                    }
+                });
+            }
+        }
+    });
+}
