9aca792c202f60a6703b800aeb28989d7f97a733
keshavsoft <nknnkumar@yahoo.com>
2025-01-19 15:22:40 +0530

    commonfuncs working

--- extension.js
+++ extension.js
@@ -1,18 +1,29 @@
 // extension.js
 const vscode = require('vscode');
 const { createFolder } = require('./src/createFolder');
+const { StartFunc: StartFuncPost } = require('./src/ClientSide/Js/Fetch/Post/entryFile');
 
-const activate = async  (context) => {
+const activate = async (context) => {
     console.log('Congratulations, your extension "create-folder" is now active!');
     // command to create a folder
     const createFolderCommand = vscode.commands.registerCommand('create-folder.createFolder', createFolder);
     context.subscriptions.push(createFolderCommand);
 
-    const clientJsFetchAsPost = vscode.commands.registerCommand('clientJs.FetchAsPost', createFolder);
+    const clientJsFetchAsPost = vscode.commands.registerCommand('clientJs.FetchAsPost', StartFuncPost);
     context.subscriptions.push(clientJsFetchAsPost);
+
+    vscode.commands.registerCommand('extension.copyToClipboard', async () => {
+        try {
+            const text = "Hello, Clipboard!";
+            await vscode.env.clipboard.writeText(text);
+            console.log("Successfully copied:", text);
+        } catch (error) {
+            console.error("Clipboard operation failed:", error);
+        }
+    });
 };
 
-function deactivate() {};
+function deactivate() { };
 
 module.exports = {
     activate,
--- src/CommonFuncs/getSelectedFolderPath.js
+++ src/CommonFuncs/getSelectedFolderPath.js
@@ -1,10 +1,9 @@
-// src/getSelectedFolderPath.js
 const vscode = require('vscode');
 const fse = require('fs-extra');
 
 const LocalFuncForFolder = async () => {
     // Try to get the currently selected folder in Explorer
-    const selectedResources = await vscode.commands.executeCommand('copyFilePath');
+    await vscode.commands.executeCommand('copyFilePath');
     const clipboardText = await vscode.env.clipboard.readText();
 
     if (clipboardText && (await fse.pathExists(clipboardText)) && (await fse.lstat(clipboardText)).isDirectory()) {
@@ -30,35 +29,6 @@ const LocalFuncForFile = async () => {
     return null;
 };
 
-async function getSelectedFolderPath1() {
-    try {
-        // Try to get the currently selected folder in Explorer
-        const selectedResources = await vscode.commands.executeCommand('copyFilePath');
-        const clipboardText = await vscode.env.clipboard.readText();
-
-        if (clipboardText && (await fse.pathExists(clipboardText)) && (await fse.lstat(clipboardText)).isDirectory()) {
-            return clipboardText;
-        }
-
-        // If no folder is selected, fall back to the active file's folder
-        const activeEditor = vscode.window.activeTextEditor;
-        if (activeEditor) {
-            const activeFilePath = activeEditor.document.uri.fsPath;
-            const activeFileFolderPath = require('path').dirname(activeFilePath);
-
-            if (await fse.pathExists(activeFileFolderPath)) {
-                return activeFileFolderPath;
-            }
-        };
-
-        // If no folder or active file is found, return null
-        return null;
-    } catch (err) {
-        vscode.window.showErrorMessage(`Error retrieving folder: ${err.message}`);
-        return null;
-    };
-};
-
 async function getSelectedFolderPath() {
     try {
         // Try to get the currently selected folder in Explorer
@@ -73,4 +43,3 @@ async function getSelectedFolderPath() {
 };
 
 module.exports = { getSelectedFolderPath };
-// main code
