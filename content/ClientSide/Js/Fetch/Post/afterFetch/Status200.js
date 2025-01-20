d81fddb05c6950c65e099a355e1e48554672bcad
keshavsoft <nknnkumar@yahoo.com>
2025-01-20 09:56:22 +0530

    FetchAsPost changed

--- content/ClientSide/Js/Fetch/Post/afterFetch/Status200.js
+++ content/ClientSide/Js/Fetch/Post/afterFetch/Status200.js
@@ -1,5 +1,15 @@
 const StartFunc = (data) => {
-    console.log("data : ", data);
+    let localAddondata = data.JsonData
+    jFLocalToInputAddonsId(localAddondata)
+};
+
+let jFLocalToInputAddonsId = (inValue) => {
+    let jVarLocalHtmlId = 'AddonsId';
+    let jVarLocalAddonsId = document.getElementById(jVarLocalHtmlId);
+
+    if (jVarLocalAddonsId === null === false) {
+        jVarLocalAddonsId.innerHTML = inValue;
+    };
 };
 
 export { StartFunc };
