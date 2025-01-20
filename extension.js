575e22ef9e78d4b852451d6bdc84ae200c82976c
Princeraj-13 <princerajchy1733@gmail.com>
2025-01-20 17:35:14 +0545

    dynamic file

--- extension.js
+++ extension.js
@@ -2,6 +2,7 @@
 const vscode = require('vscode');
 const { createFolder } = require('./src/createFolder');
 const { StartFunc: StartFuncPost } = require('./src/ClientSide/Js/Fetch/Post/entryFile');
+const { StartFuncDynamic: StartFuncPostDynamic } = require('./src/dynamicFile');
 const { ConfigFunc: CopyConfig } = require('./src/configFile');
 
 const activate = async (context) => {
@@ -12,6 +13,9 @@ const activate = async (context) => {
 
     const clientJsFetchAsPost = vscode.commands.registerCommand('clientJs.FetchAsPost', StartFuncPost);
     context.subscriptions.push(clientJsFetchAsPost);
+
+    const dynamicFetchAsPost = vscode.commands.registerCommand('dynamic.FetchAsPost', StartFuncPostDynamic);
+    context.subscriptions.push(dynamicFetchAsPost);
 
     const configCopyConfigFile = vscode.commands.registerCommand('config.CopyConfigFile', CopyConfig);
     context.subscriptions.push(configCopyConfigFile);
