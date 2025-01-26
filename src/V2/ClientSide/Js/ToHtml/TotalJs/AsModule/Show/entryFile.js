a88630abb86f625f6e3318913bd3843aba2de150
keshavsoft <nknnkumar@yahoo.com>
2025-01-26 15:36:38 +0530

    Show added

--- src/V2/ClientSide/Js/ToHtml/TotalJs/AsModule/Show/entryFile.js
+++ src/V2/ClientSide/Js/ToHtml/TotalJs/AsModule/Show/entryFile.js
@@ -1,36 +1,9 @@
-const vscode = require('vscode');
-const fse = require('fs-extra');
-const path = require('path');
-
-const { getSelectedFolderPath } = require('../../../../../../../CommonFuncs/getSelectedFolderPath');
-const CommonRegisterCommand = "ClientSide.Js.ToHtml.TotalJs.AsModule";
+const { StartFunc: StartFuncUrlFromJson } = require("./UrlFromJson/entryFile");
+const { StartFunc: StartFuncUrlFromInput } = require("./UrlFromInput/entryFile");
 
 const StartFunc = () => {
-    activateFunc();
-};
-
-const activateFunc = () => {
-    vscode.commands.registerCommand(CommonRegisterCommand, LocalFuncToActivate);
-};
-
-const LocalFuncToActivate = async () => {
-    try {
-        const selectedFolder = await getSelectedFolderPath();
-
-        if (!selectedFolder) throw new Error('No folder selected, and no active file found in the workspace.');
-
-        vscode.window.showInformationMessage(`------------- folder: ${__dirname}`);
-
-        const LocalFromPath = path.join(__dirname, "copyCode");
-        const LocalToPath = path.join(selectedFolder, "Show");
-
-        await fse.copy(LocalFromPath, LocalToPath);
-
-    } catch (error) {
-        console.log("aaaaaaa  : ", error.message);
-
-        vscode.window.showErrorMessage(`Error: ${error.message}`);
-    };
+    StartFuncUrlFromJson();
+    StartFuncUrlFromInput();
 };
 
 module.exports = { StartFunc };
