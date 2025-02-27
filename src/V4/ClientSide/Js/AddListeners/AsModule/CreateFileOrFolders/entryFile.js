2beffba110f2d3e3eaa4026e7e5ddb2fc759bba6
keshavsoft <nknnkumar@yahoo.com>
2025-02-27 10:59:47 +0530

    Check still neet to be done more

--- src/V4/ClientSide/Js/AddListeners/AsModule/CreateFileOrFolders/entryFile.js
+++ src/V4/ClientSide/Js/AddListeners/AsModule/CreateFileOrFolders/entryFile.js
@@ -1,9 +1,9 @@
 const { StartFunc: StartFuncAddListeners } = require("./AddListeners/entryFile");
-const { StartFunc: StartFuncToAddListenersFile } = require("./toAddListenersFile");
+const { StartFunc: StartFuncToAddListenersFile } = require("./ToAddListenersFile/entryFile");
 
 const StartFunc = async ({ inCreatePath, inHtmlIdNeeded }) => {
     await StartFuncAddListeners({ inCreatePath, inHtmlIdNeeded });
-    await StartFuncToAddListenersFile({ inCreatePath, inHtmlIdNeeded });
+    await StartFuncToAddListenersFile({ inAlterPath: inCreatePath, inHtmlIdNeeded });
 };
 
 module.exports = { StartFunc };
--- src/V4/ClientSide/Js/AddListeners/AsModule/CreateFileOrFolders/toAddListenersFile.js
+++ src/V4/ClientSide/Js/AddListeners/AsModule/CreateFileOrFolders/toAddListenersFile.js
@@ -21,11 +21,11 @@ const LocalFuncCheckFile = async ({ inCreatePath, inHtmlIdNeeded }) => {
 
         vscode.window.showInformationMessage(`${LocalPathNeeded} folder is already present!`);
     } catch (err) {
-        LocalCreateFile({ inCreatePath: LocalToCreatePath, inHtmlIdNeeded: LocalHtmlIdNeeded });
+        LocalFuncCreateFile({ inCreatePath: LocalToCreatePath, inHtmlIdNeeded: LocalHtmlIdNeeded });
     };
 };
 
-const LocalCreateFile = async ({ inCreatePath, inHtmlIdNeeded }) => {
+const LocalFuncCreateFile = async ({ inCreatePath, inHtmlIdNeeded }) => {
     const LocalToCreatePath = inCreatePath;
     const LocalHtmlIdNeeded = inHtmlIdNeeded;
 
@@ -40,6 +40,38 @@ const LocalCreateFile = async ({ inCreatePath, inHtmlIdNeeded }) => {
     ];
 
     await fs.writeFileSync(`${LocalToCreatePath}/AddListeners/entryFile.js`, LocalContentToInsert.join('\n'));
+};
+
+const LocalFuncAlterFile = async ({ inCreatePath, inHtmlIdNeeded }) => {
+    const LocalToCreatePath = inCreatePath;
+    const LocalHtmlIdNeeded = inHtmlIdNeeded;
+    const LocalPathNeeded = `${LocalToCreatePath}/AddListeners/entryFile.js`;
+
+    const fetchFuncsContent = await fse.readFile(fetchFuncsPath, 'utf8');
+
+    const LocalContentToInsert = [
+        `import { StartFunc as StartFuncAddListeners } from './${LocalHtmlIdNeeded}/entryFile.js';`,
+        "",
+        "let StartFunc = () => {",
+        `\tStartFuncAddListeners();`,
+        "};",
+        "",
+        "export { StartFunc };"
+    ];
+
+    await fs.writeFileSync(LocalPathNeeded, LocalContentToInsert.join('\n'));
+};
+
+async function updateFetchFuncs(fetchFuncsPath, configJsonPath) {
+    const fetchFuncsContent = await fse.readFile(fetchFuncsPath, 'utf8');
+    const relativePath = path.relative(path.dirname(fetchFuncsPath), configJsonPath).replace(/\\/g, '/');
+
+    const updatedContent = fetchFuncsContent.replace(
+        /import ConfigJson from '.*Config\.json' with \{type: 'json'\};/,
+        `import ConfigJson from '${relativePath}' with {type: 'json'};`
+    );
+
+    await fse.writeFile(fetchFuncsPath, updatedContent, 'utf8');
 };
 
 module.exports = { StartFunc };
