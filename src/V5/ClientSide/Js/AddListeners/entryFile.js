d23b531782a6ce63c6251fe3cb5b758337a29b98
keshavsoft <nknnkumar@yahoo.com>
2025-03-19 18:55:32 +0530

    new addlisteners by html id working good

--- src/V5/ClientSide/Js/AddListeners/entryFile.js
+++ src/V5/ClientSide/Js/AddListeners/entryFile.js
@@ -1,5 +1,9 @@
 const { StartFunc: StartFuncAsModule } = require("./AsModule/entryFile");
+const { StartFunc: StartFuncFromToHtmlId } = require("./ToHtmlId/entryFile");
 
-const StartFunc = StartFuncAsModule;
+const StartFunc = () => {
+    StartFuncAsModule();
+    StartFuncFromToHtmlId();
+};
 
 module.exports = { StartFunc };
--- src/V5/ClientSide/Js/ToHtml/TotalJs/AsModule/Show/ColsFromFetAftDest/entryFile.js
+++ src/V5/ClientSide/Js/ToHtml/TotalJs/AsModule/Show/ColsFromFetAftDest/entryFile.js
@@ -1,9 +1,7 @@
 const { StartFunc: StartFuncUrlFromJson } = require("./UrlFromJson/entryFile");
-const { StartFunc: StartFuncUrlFromInput } = require("./UrlFromInput/entryFile");
 
 const StartFunc = () => {
     StartFuncUrlFromJson();
-    StartFuncUrlFromInput();
 };
 
 module.exports = { StartFunc };
