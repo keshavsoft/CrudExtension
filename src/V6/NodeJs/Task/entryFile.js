f4a651b21ea289c2921b999172f1c7b76b7df31a
keshavsoft <nknnkumar@yahoo.com>
2025-03-24 22:01:54 +0530

    selected columns working

--- src/V6/NodeJs/Task/entryFile.js
+++ src/V6/NodeJs/Task/entryFile.js
@@ -1,5 +1,9 @@
 const { StartFunc: StartFuncFromNewTask } = require("./NewTask/entryFile");
+const { StartFunc: StartFuncFromSelectedColumns } = require("./SelectedColumns/entryFile");
 
-const StartFunc = StartFuncFromNewTask;
+const StartFunc = () => {
+    StartFuncFromNewTask();
+    StartFuncFromSelectedColumns();
+};
 
 module.exports = { StartFunc };
