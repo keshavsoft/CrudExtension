1855cbb5cbc2b74e6e18a6e3075ceb56ada5ea95
keshavsoft <nknnkumar@yahoo.com>
2025-03-31 12:33:24 +0530

    FetchAsGet new methods started in v9

--- src/V3/ClientSide/Js/entryFile.js
+++
@@ -1,9 +0,0 @@
-const { StartFunc: StartFuncToHtml } = require("./ToHtml/entryFile");
-const { StartFunc: StartFuncFetchFuncs } = require("./FetchFuncs/entryFile");
-
-const StartFunc = () => {
-    StartFuncToHtml();
-    StartFuncFetchFuncs();
-};
-
-module.exports = { StartFunc };
