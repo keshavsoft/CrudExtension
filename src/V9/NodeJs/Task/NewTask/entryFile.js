e30212ce0fdc209a83daf00c7619fb40e237b86c
keshavsoft <nknnkumar@yahoo.com>
2025-04-01 16:38:49 +0530

    AlterPut added

--- src/V9/NodeJs/Task/NewTask/entryFile.js
+++ src/V9/NodeJs/Task/NewTask/entryFile.js
@@ -1,9 +1,11 @@
 const { StartFunc: StartFuncCreate } = require("./Create/entryFile");
 const { StartFunc: StartFuncFromShowGet } = require("./ShowGet/entryFile");
+const { StartFunc: StartFuncFromAlterPut } = require("./AlterPut/entryFile");
 
 const StartFunc = () => {
     StartFuncCreate();
     StartFuncFromShowGet();
+    StartFuncFromAlterPut();
 };
 
 module.exports = { StartFunc };
