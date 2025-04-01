d8a5134ea241be6a2f042177b4a086d287104cdb
keshavsoft <nknnkumar@yahoo.com>
2025-04-01 18:13:29 +0530

    AggrFuncsGet done

--- src/V9/NodeJs/Task/NewTask/entryFile.js
+++ src/V9/NodeJs/Task/NewTask/entryFile.js
@@ -1,11 +1,13 @@
 const { StartFunc: StartFuncCreate } = require("./Create/entryFile");
 const { StartFunc: StartFuncFromShowGet } = require("./ShowGet/entryFile");
 const { StartFunc: StartFuncFromAlterPut } = require("./AlterPut/entryFile");
+const { StartFunc: StartFuncFromAggrFuncsGet } = require("./AggrFuncsGet/entryFile");
 
 const StartFunc = () => {
     StartFuncCreate();
     StartFuncFromShowGet();
     StartFuncFromAlterPut();
+    StartFuncFromAggrFuncsGet();
 };
 
 module.exports = { StartFunc };
