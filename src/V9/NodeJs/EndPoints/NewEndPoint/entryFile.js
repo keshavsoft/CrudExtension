8187163081d39eca7640e216c36a3ef8cef43996
nknnkumar <115708070+nknnkumar@users.noreply.github.com>
2025-04-02 11:17:55 +0530

    CreateNoSync added

--- src/V9/NodeJs/EndPoints/NewEndPoint/entryFile.js
+++ src/V9/NodeJs/EndPoints/NewEndPoint/entryFile.js
@@ -1,9 +1,11 @@
 const { StartFunc: StartFuncCreate } = require("./Create/entryFile");
 const { StartFunc: StartFuncCheck } = require("./Check/entryFile");
+const { StartFunc: StartFuncFromCreateNoSync } = require("./CreateNoSync/entryFile");
 
 const StartFunc = () => {
     StartFuncCreate();
     StartFuncCheck();
+    StartFuncFromCreateNoSync();
 };
 
 module.exports = { StartFunc };
