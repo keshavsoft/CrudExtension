5b66c8188adc96dc05111063cb9943e2f504196c
keshavsoft <nknnkumar@yahoo.com>
2025-04-10 18:03:27 +0530

    CommonConfig started

--- src/V11/ClientSide/Js/ToHtml/TotalJs/entryFile.js
+++ src/V11/ClientSide/Js/ToHtml/TotalJs/entryFile.js
@@ -1,5 +1,9 @@
 const { StartFunc: StartFuncAsModule } = require("./AsModule/entryFile");
+const { StartFunc: StartFuncFromCommonConfig } = require("./CommonConfig/entryFile");
 
-const StartFunc = StartFuncAsModule;
+const StartFunc = () => {
+    StartFuncAsModule();
+    StartFuncFromCommonConfig();
+};
 
 module.exports = { StartFunc };
